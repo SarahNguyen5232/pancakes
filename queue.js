b81ef9002b258e7b3cc657734b69e7ee89e461fd
SarahNguyen5232 <148515397+SarahNguyen5232@users.noreply.github.com>
2024-08-09 00:31:37 -0700

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+  }
+  dequeue() {
+  }
+  peek() {
+  }
+  isEmpty() {
+
+  }
+}
+
+module.exports = Queue;
